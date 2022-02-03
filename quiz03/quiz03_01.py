7dd94154b5e807b84177ca6dbb089aced3f2a196
Broco98 <qkrgydus00@naver.com>
2022-02-03 21:37:43 +0900

    3장문제 추가

---
+++ quiz03/quiz03_01.py
@@ -0,0 +1,7 @@
+a = "Life is too shorts, you need python"
+
+if "wife" in a : print("wife")
+elif "pythin" in a and "you" not in a : print("python")
+elif "shirt" not in a : print("shirt")
+elif "need" in a : print("need")
+else: print("none")
---
+++ quiz03/quiz03_03.py
@@ -0,0 +1,5 @@
+i = 0
+while True:
+    i += 1
+    if i > 5: break
+    print("*"*i) # 문자열 * 숫자 = 반복
